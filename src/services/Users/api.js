d8b6178b2558a4a0d9eb523556ee12c5b897a23d
Jad Sarout <xananax@yelostudio.com>
2018-03-08 23:27:50 +0200

    razzle github creds

--- src/services/Users/api.js
+++ src/services/Users/api.js
@@ -3,9 +3,6 @@ import passport from 'passport'
 //import { Strategy as LocalStrategy } from 'passport-local'
 import { Strategy as GithubStrategy } from 'passport-github2'
 
-const GITHUB_CLIENT_ID = "--insert-github-client-id-here--";
-const GITHUB_CLIENT_SECRET = "--insert-github-client-secret-here--";
-
 // Passport session setup.
 //   To support persistent login sessions, Passport needs to be able to
 //   serialize users into and deserialize users out of the session.  Typically,
@@ -13,34 +10,40 @@ const GITHUB_CLIENT_SECRET = "--insert-github-client-secret-here--";
 //   the user by ID when deserializing.  However, since this example does not
 //   have a database of user records, the complete GitHub profile is serialized
 //   and deserialized.
-passport.serializeUser( ( user, done ) => done( null, user ) )
-passport.deserializeUser( ( obj, done ) => done( null, obj ) )
+passport.serializeUser((user, done) => done(null, user))
+passport.deserializeUser((obj, done) => done(null, obj))
 
-passport.use(new GitHubStrategy({
-  clientID: GITHUB_CLIENT_ID,
-  clientSecret: GITHUB_CLIENT_SECRET,
-  callbackURL: "http://127.0.0.1:3000/auth/github/callback"
-},
-(accessToken, refreshToken, profile, done) => {
-  process.nextTick( () => {
-    // To keep the example simple, the user's GitHub profile is returned to
-    // represent the logged-in user.  In a typical application, you would want
-    // to associate the GitHub account with a user record in your database,
-    // and return that user instead.
-    return done(null, profile);
-  });
+const github_callback_url = '/auth/github/callback'
+
+const { RAZZLE_GITHUB_CLIENT_SECRET:clientID, RAZZLE_GITHUB_CLIENT_SECRET:clientSecret, HOST, PORT } = env
+
+const github_options = {
+  clientID,
+  clientSecret,
+  callbackURL: `http://${HOST}:${PORT}${github_callback_url}`
 }
+
+passport.use(new GitHubStrategy(github_options,
+  ( accessToken, refreshToken, profile, done ) => {
+    process.nextTick(() => {
+      // To keep the example simple, the user's GitHub profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the GitHub account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
 ));
 
 const app = new Router()
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/account', ensureAuthenticated, function(req, res){
+app.get('/account', ensureAuthenticated, function (req, res) {
   res.render('account', { user: req.user });
 });
 
-app.get('/login', function(req, res){
+app.get('/login', function (req, res) {
   res.render('login', { user: req.user });
 });
 
@@ -50,8 +53,8 @@ app.get('/login', function(req, res){
 //   the user to github.com.  After authorization, GitHub will redirect the user
 //   back to this application at /auth/github/callback
 app.get('/auth/github',
-  passport.authenticate('github', { scope: [ 'user:email' ] }),
-  function(req, res){
+  passport.authenticate('github', { scope: ['user:email'] }),
+  function (req, res) {
     // The request will be redirected to GitHub for authentication, so this
     // function will not be called.
   });
@@ -61,13 +64,13 @@ app.get('/auth/github',
 //   request.  If authentication fails, the user will be redirected back to the
 //   login page.  Otherwise, the primary route function will be called,
 //   which, in this example, will redirect the user to the home page.
-app.get('/auth/github/callback', 
+app.get(github_callback_url,
   passport.authenticate('github', { failureRedirect: '/login' }),
-  function(req, res) {
+  function (req, res) {
     res.redirect('/');
   });
 
-app.get('/logout', function(req, res){
+app.get('/logout', function (req, res) {
   req.logout();
   res.redirect('/');
 });
