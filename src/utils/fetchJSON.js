76043f97dd6d92bbb441afbc17756197e2fdb982
Xananax <xananax@yelostudio.com>
2018-03-13 17:46:33 +0200

    fetch improvement

--- src/utils/fetchJSON.js
+++ src/utils/fetchJSON.js
@@ -1,11 +1,46 @@
+/**
+ * This function uses `fetch`, present in all modern browsers,
+ * to load a url, transform the json response to a javascript object.
+ * This object is assumed to be of the form:
+ * 
+ * ```js
+ * {data:anything}
+ * // or
+ * {error:true, data:string}
+ * ```
+ * Then, if the javacript object has a "error" property, then fetch
+ * will throw an error with the data message.
+ * If not, it will return the data itself
+ * @param {string} url 
+ */
 export const fetchJSON = (url) =>
   fetch(url)
-    .then(response => response.json())
     .then(response => {
-      if (response.error) {
-        throw new Error(response.data)
-      }
-      return response.data
+      const status = response.status
+      return response.json()
+        .then(json => {
+          // all ok
+          if (status <= 400 && !json.error) {
+            return json.data
+          }
+          // not ok, and
+          // an error message was provided
+          if (json && ('data' in json) && (typeof json.data === 'string')) {
+            throw new Error(json.data)
+          }
+          // not ok, and
+          // no error message was provided
+          if (status >= 500) {
+            throw new Error('generic error')
+          }
+          if (status >= 400) {
+            throw new Error(`url not found: '${url}'`)
+          }
+          throw new Error(`no error message and status '${status}' unknown`)
+
+        })
+        .catch(err => { throw err })
     })
+    .catch(err => { throw err })
 
 export default fetchJSON
