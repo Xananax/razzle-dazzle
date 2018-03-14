cebd713f18bae5f15cbd58db9efd4d7ad353f986
xananax <xananax@yelostudio.com>
2018-03-14 08:06:34 +0200

    better blog schema

--- src/services/Blog/migrations/20180311220414_blog.js
+++ src/services/Blog/migrations/20180311220414_blog.js
@@ -1,46 +1,40 @@
 
-exports.up = function(knex, Promise) {
-	return Promise.all([
-        knex.schema.createTable('users', function(table) {
-            table.increments('uid').primary();
-            table.string('username').notNullable()
-            table.string('password').notNullable()
-            table.string('name');
-            table.string('email');
-            table.timestamps(false, true);
-        }),
+exports.up = (knex, Promise) =>
+  Promise.all([
+    knex.schema.createTable('users', (table) => {
+      table.increments('user_id').primary();
+      table.string('email').notNullable()
+      table.string('password').notNullable()
+      table.string('nick');
+      table.timestamps(true, true);
+    }),
 
-        knex.schema.createTable('posts', function(table){
-            table.increments('id').primary();
-            table.string('title');
-            table.string('body');
-            table.integer('author_id')
-                 .references('uid')
-                 .inTable('users');
-            table.dateTime('postDate');
-        }),
+    knex.schema.createTable('posts', (table) => {
+      table.increments('post_id').primary();
+      table.string('title');
+      table.string('body');
+      table.integer('user_id')
+        .references('user_id')
+        .inTable('users');
+      table.timestamps(true, true);
+    }),
 
-        knex.schema.createTable('comments', function(table){
-            table.increments('id').primary();
-            table.string('body');
-            table.integer('author_id')
-                 .references('uid')
-                 .inTable('users');
-            table.integer('post_id')
-                 .references('id')
-                 .inTable('posts');
-            table.dateTime('postDate');
-        })
-    ])
-  
-};
+    knex.schema.createTable('comments', (table) => {
+      table.increments('comment_id').primary();
+      table.string('body');
+      table.integer('user_id')
+        .references('user_id')
+        .inTable('users');
+      table.integer('post_id')
+        .references('id')
+        .inTable('posts');
+      table.timestamps(true, true);
+    })
+  ])
 
-exports.down = function(knex, Promise) {
-
-	return Promise.all([
-        knex.schema.dropTable('users'),
-        knex.schema.dropTable('posts'),
-        knex.schema.dropTable('comments')
-    ])
-  
-};
+exports.down = (knex, Promise) =>
+  Promise.all([
+    knex.schema.dropTable('users'),
+    knex.schema.dropTable('posts'),
+    knex.schema.dropTable('comments')
+  ])
